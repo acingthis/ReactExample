c4120e094494b19fd07ed3d3441dafb224835764
Robert <acing-psn@outlook.com>
2022-11-11 10:57:43 +0000

    Added Example tests

--- src/tests/todo.test.js
+++ src/tests/todo.test.js
@@ -1,3 +1,4 @@
+/* 
 import{ render, screen, cleanup} from '@testing-library/react';
 import renderer  from 'react-test-renderer';
 import '@testing-library/jest-dom'
@@ -5,8 +6,6 @@ import '@testing-library/jest-dom'
 afterEach(() => {
     cleanup();
 });
-
-/* 
 
 // Example test
 
